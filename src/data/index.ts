91c57caeac413118e27f8376d58891f1b31a8091
Nadine <nadlgit@users.noreply.github.com>
2023-05-16 20:27:59 -0400

    feat: data types and getters

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,6 +1,27 @@
 import data from './data.json';
 
-export const jobs = data.map((job) => ({
+export type JobInfo = (typeof data)[number];
+
+export type JobSummary = Pick<
+  JobInfo,
+  'id' | 'company' | 'logo' | 'logoBackground' | 'position' | 'postedAt' | 'contract' | 'location'
+>;
+
+const jobs: JobInfo[] = data.map((job) => ({
   ...job,
   logo: job.logo.replace('./assets/logos/', '/logos/'),
 }));
+
+export const getJobSummaries = () =>
+  jobs.map(({ id, company, logo, logoBackground, position, postedAt, contract, location }) => ({
+    id,
+    company,
+    logo,
+    logoBackground,
+    position,
+    postedAt,
+    contract,
+    location,
+  }));
+
+export const getJob = (jobId: JobInfo['id']) => jobs.find(({ id }) => id === jobId);
