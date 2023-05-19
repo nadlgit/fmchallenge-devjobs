8760fa1f9ad750626a732f15864c2d97cfb82a7a
Nadine <nadlgit@users.noreply.github.com>
2023-05-18 22:57:28 -0400

    feat: styled job detail middle

--- src/ui/components/jobs/detail-section.tsx
+++ src/ui/components/jobs/detail-section.tsx
@@ -1,20 +1,44 @@
-import { ListItem, OrderedList, Text } from '@chakra-ui/react';
+import { List, ListItem, Text } from '@chakra-ui/react';
 
 type DetailSectionProps = {
   title: string;
   data: { content: string; items: string[] };
+  isNumbered?: boolean;
 };
 
-export const DetailSection = ({ title, data }: DetailSectionProps) => (
+export const DetailSection = ({ title, data, isNumbered }: DetailSectionProps) => (
   <>
-    <Text as="h2" fontSize="1.25rem" lineHeight="1.5rem" fontWeight="bold" color="text.contrast">
+    <Text
+      as="h2"
+      fontSize="1.25rem"
+      lineHeight="1.5rem"
+      fontWeight="bold"
+      color="text.contrast"
+      mt="2.5rem"
+    >
       {title}
     </Text>
-    <Text>{data.content}</Text>
-    <OrderedList>
+
+    <Text my="1.5rem">{data.content}</Text>
+
+    <List as={isNumbered ? 'ol' : 'ul'} spacing="0.5rem">
       {data.items.map((item, idx) => (
-        <ListItem key={`req${idx}`}>{item}</ListItem>
+        <ListItem
+          key={`req${idx}`}
+          pl="2.25rem"
+          pos="relative"
+          _before={{
+            pos: 'absolute',
+            ml: '-2.25rem',
+            content: isNumbered ? `"${idx + 1}"` : '"\u2022"',
+            fontSize: isNumbered ? undefined : '1.5em',
+            fontWeight: 'bold',
+            color: 'violet',
+          }}
+        >
+          {item}
+        </ListItem>
       ))}
-    </OrderedList>
+    </List>
   </>
 );
--- src/ui/components/jobs/job-detail.tsx
+++ src/ui/components/jobs/job-detail.tsx
@@ -22,29 +22,36 @@ export const JobDetail = ({
   description,
   requirements,
   role,
-}: JobDetailProps) => {
-  return (
-    <Stack mt="-2.5rem">
-      <Company company={company} logo={logo} logoBackground={logoBackground} website={website} />
+}: JobDetailProps) => (
+  <Stack spacing="clamp(1.5rem, calc(2.04vw + 1.02rem), 2rem)" mt="-2.5rem">
+    <Company company={company} logo={logo} logoBackground={logoBackground} website={website} />
 
-      <Box bgColor="bg.base">
-        <Flex direction={{ base: 'column', tablet: 'row' }}>
-          <Box>
-            <Auxiliary postedAt={postedAt} contract={contract} />
-            <Position position={position} as="h1" isResponsive />
-            <Location location={location} />
-          </Box>
-          <Apply apply={apply} />
-        </Flex>
+    <Box
+      px="clamp(1.5rem, calc(6.1vw + 0.07rem), 3rem)"
+      py="clamp(2.5rem, calc(2.04vw + 2.02rem), 3rem)"
+      bgColor="bg.base"
+      borderRadius="0.375rem"
+    >
+      <Flex
+        direction={{ base: 'column', tablet: 'row' }}
+        justify="space-between"
+        align={{ base: 'unset', tablet: 'center' }}
+      >
+        <Stack spacing="0.5rem" mb={{ base: '3rem', tablet: 'unset' }}>
+          <Auxiliary postedAt={postedAt} contract={contract} />
+          <Position position={position} as="h1" isResponsive />
+          <Location location={location} />
+        </Stack>
+        <Apply apply={apply} />
+      </Flex>
 
-        <Text>{description}</Text>
+      <Text my="clamp(2rem, calc(2.04vw + 1.52rem), 2.5rem)">{description}</Text>
 
-        <DetailSection title="Requirements" data={requirements} />
+      <DetailSection title="Requirements" data={requirements} />
 
-        <DetailSection title="What You Will Do" data={role} />
-      </Box>
+      <DetailSection title="What You Will Do" data={role} isNumbered />
+    </Box>
 
-      <Apply apply={apply} position={position} company={company} />
-    </Stack>
-  );
-};
+    <Apply apply={apply} position={position} company={company} />
+  </Stack>
+);
